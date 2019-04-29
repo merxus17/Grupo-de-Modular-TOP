e25691fa17bec91ef2eb96cf6f2196c8958e0c52
rodrigo <rodrigomonteiropereira1@gmail.com>
2019-04-29 12:48:20 -0300

    Introdução do modulo tabuleiro

---
+++ Modulo-Tabuleiro/Tabuleiro.h
@@ -0,0 +1,42 @@
+$MCD M�dulo de defini��o: Tabuleiro do jogo de Ludo
+*
+*  Arquivo gerado:              Tabuleiro.h
+*  Letras identificadoras:      TAB
+*
+*  Projeto: INF 1301 / 1628 Automatiza��o dos testes de m�dulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: avs
+*
+*  $HA Hist�rico de evolu��o:
+*     Vers�o  Autor    Data     Observa��es
+*     1       avs   29/abr/2019 in�cio desenvolvimento
+*
+*  $ED Descri��o do m�dulo
+*     Implementa listas gen�ricas duplamente encadeadas.
+*     Podem existir n listas em opera��o simultaneamente.
+*     As listas possuem uma cabe�a encapsulando o seu estado.
+*
+*     Cada lista � homog�nea quanto ao tipo dos dados que armazena.
+*     Cada elemento da lista referencia o valor que cont�m.
+*
+*     Os ponteiros para os dados s�o copiados para elementos da lista.
+*        N�o � copiado o valor apontado por estes ponteiros.
+*
+*     O controle da destrui��o do valor de um elemento a ser exclu�do
+*        � realizado por uma fun��o fornecida pelo usu�rio.
+*
+*     Cada lista referencia uma fun��o que determina como devem ser
+*        desalocados os dados nela contidos.
+*
+*     A fun��o de libera��o dos valores contidos nos elementos deve
+*        assegurar a libera��o de todos os espa�os refer�nciados pelo
+*        valor contido em um elemento.
+*        Esta fun��o � chamada antes de se desalocar um elemento
+*        de uma lista.
+*        Caso n�o seja necess�rio desalocar o valor referenciado pelo
+*        elemento, o ponteiro para a fun��o de libera��o poder� ser NULL .
+*        Caso o elemento da lista seja a �nica �ncora do valor referenciado,
+*        esta fun��o deve promover a destrui��o (free) desse valor e
+*        de todos os dados nele ancorados.
+*
+***************************************************************************/
